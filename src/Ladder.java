583aba97a3444073e7d465a860aff6f0e58b6be0
champ.ion <champ.ion@kakaocorp.com>
2021-12-28 00:40:50 +0900

    Ladder Game STEP1 - Feedback(PR#131) - [Refactoring] Changed the name of the instance variable

--- src/Ladder.java
+++ src/Ladder.java
@@ -3,14 +3,14 @@ import java.util.Random;
 public class Ladder {
     private final int width;
     private final int height;
-    private final boolean[][] bridge;
+    private final boolean[][] bridges;
 
     private static final int WIDTH_CORRECTION = 1;
 
     public Ladder(int numPeople, int ladderHeight) {
         this.width = numPeople - WIDTH_CORRECTION;
         this.height = ladderHeight;
-        this.bridge = new boolean[height][width];
+        this.bridges = new boolean[height][width];
         initializeBridge();
     }
 
@@ -19,7 +19,7 @@ public class Ladder {
 
         for (int h = 0; h < height; h++) {
             for (int w = 0; w < width; w++) {
-                bridge[h][w] = RANDOM.nextBoolean();
+                bridges[h][w] = RANDOM.nextBoolean();
             }
         }
     }
@@ -30,7 +30,7 @@ public class Ladder {
         for (int h = 0; h < height; h++) {
             ladder.append("|");
             for (int w = 0; w < width; w++) {
-                ladder.append(bridge[h][w] ? "-" : " ").append("|");
+                ladder.append(bridges[h][w] ? "-" : " ").append("|");
             }
             ladder.append("\n");
         }
