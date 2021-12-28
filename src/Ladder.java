a9c3bc1b000c0a0b97e7c0a347d74f0f4253a1a6
champ.ion <champ.ion@kakaocorp.com>
2021-12-28 11:16:21 +0900

    Ladder Game STEP2 - [Refactoring] Reduce indentation

--- src/Ladder.java
+++ src/Ladder.java
@@ -1,11 +1,12 @@
 import java.util.Random;
+import java.util.StringJoiner;
 
 public class Ladder {
+    private static final int WIDTH_CORRECTION = 1;
+
     private final int width;
     private final int height;
     private final boolean[][] bridges;
-
-    private static final int WIDTH_CORRECTION = 1;
 
     public Ladder(int numPeople, int ladderHeight) {
         this.width = numPeople - WIDTH_CORRECTION;
@@ -15,12 +16,16 @@ public class Ladder {
     }
 
     private void initializeBridge() {
+        for (int h = 0; h < height; h++) {
+            initializeRow(h);
+        }
+    }
+
+    private void initializeRow(int rowIdx) {
         final Random RANDOM = new Random();
 
-        for (int h = 0; h < height; h++) {
-            for (int w = 0; w < width; w++) {
-                bridges[h][w] = RANDOM.nextBoolean();
-            }
+        for (int w = 0; w < width; w++) {
+            bridges[rowIdx][w] = RANDOM.nextBoolean();
         }
     }
 
@@ -28,12 +33,16 @@ public class Ladder {
     public String toString() {
         StringBuilder ladder = new StringBuilder();
         for (int h = 0; h < height; h++) {
-            ladder.append("|");
-            for (int w = 0; w < width; w++) {
-                ladder.append(bridges[h][w] ? "-" : " ").append("|");
-            }
-            ladder.append("\n");
+            ladder.append(buildRow(h)).append("\n");
         }
         return ladder.toString();
+    }
+
+    private String buildRow(int rowIdx) {
+        StringJoiner row = new StringJoiner("|", "|", "|");
+        for (int w = 0; w < width; w++) {
+            row.add(bridges[rowIdx][w] ? "-" : " ");
+        }
+        return row.toString();
     }
 }
